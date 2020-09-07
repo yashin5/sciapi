c276dd17cec1da6035abd8c082fb8453293ecd94
Cecchetti <pedro.cecchetti@gmail.com>
2020-09-08 01:12:08 +0200

    comply on changes from feat/change-ts-config

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-var express = require('express');
+import express from 'express'
+import { Request, Response } from 'express'
+import person from './routes/scientist'
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +9,8 @@ const HOST = '0.0.0.0';
 app.get('/',function(req,res){
     res.send("Site de Tecnologia");
 });
+
+app.use("/scientist", person)
 
 app.listen(port,function(){
     console.log(`Running on http://${HOST}:${port}`);
