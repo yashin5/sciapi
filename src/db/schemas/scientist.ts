1ffd128137370405aec890ffcb8d3fd2feed29c3
Cecchetti <pedro.cecchetti@gmail.com>
2020-10-23 18:41:12 +0200

    feat: Improved ScientistSchema for better validation

--- src/db/schemas/scientist.ts
+++ src/db/schemas/scientist.ts
@@ -1,14 +1,15 @@
-import mongoose from '../config'
+import mongoose from "../config";
 const { Schema } = mongoose;
 
 const ScientistSchema = new Schema({
-  name:  {type: String, unique: true},
-  DOB: Date,
-  nationality: String,
-  cityOfBirth: String,
-  fields: [String],
-  nobelPrize: Boolean,
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  DOB: { type: Date, required: true },
+  nationality: { type: String, required: true },
+  cityOfBirth: { type: String, required: true },
+  fields: [{ type: String }],
+  nobelPrize: { type: Boolean, default: false },
 });
 
-const Scientist = mongoose.model('Scientist', ScientistSchema)
+const Scientist = mongoose.model("Scientist", ScientistSchema);
 export default Scientist;
