7ff30e6eeb884ed1107177e401b7c9bd1bf63a8f
Cecchetti <pedro.cecchetti@gmail.com>
2020-10-23 17:48:59 +0200

    feat: Added json parser as a middleware to the application

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
 import express from 'express'
 import { Request, Response } from 'express'
-import person from './routes/scientist'
+import scientistsRouter from './routes/scientist'
 
 
 var app = express();
 const port = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
-app.get('/',function(req: Request,res: Response){
-    res.send("Site de Tecnologia");
-});
+// Defining Middleware
+app.use(express.json())
+app.use("/scientist", scientistsRouter)
 
-app.use("/scientist", person)
+
+app.get('/',function(req: Request,res: Response){
+
+    const information = {
+        project: "SciAPI",
+        message: "Welcome to the Scientist API"
+    }
+
+    res.send(information)
+});
 
 app.listen(port,function(){
     console.log(`Running on http://${HOST}:${port}`);
