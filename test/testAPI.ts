1f417d9117a5b5f0f0f6be4e4e58f98c66308fa1
Cecchetti <pedro.cecchetti@gmail.com>
2020-11-03 16:49:40 +0100

    test: Created first tests for the application

---
+++ test/testAPI.ts
@@ -0,0 +1,49 @@
+import { doesNotMatch } from 'assert';
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import app from '../src/server'
+
+chai.use(chaiHttp);
+chai.should();
+
+describe('API Index Endpoint', () => {
+
+    it('should get welcome message', () => {
+        chai.request(app)
+        .get("/")
+        .end((err,res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.message.should.include('Welcome to the Scientist API')
+        })
+    });
+});
+
+
+describe('API Scientists Endpoint', () => {
+
+    const marieCurie = {
+        firstName: "Marie",
+        lastName: "Curie",
+        nationality: "polish",
+        fields: [
+            "chemistry",
+            "physics",
+            "radioactivity"
+        ],
+        nobelPrize: true,
+        DOB: "1867-11-07",
+    }
+
+    it('Should Not Create Scientist with incomplete information', () => {
+        chai.request(app)
+        .post("/scientist/")
+        .type('application/json')
+        .send(marieCurie)
+        .end((err, res) => {
+            res.should.have.status(400)
+        })
+    })
+
+    
+})
