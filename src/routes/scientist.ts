0d05a51eb3018252e6c006299afe1df0f955d94e
Cecchetti <pedro.cecchetti@gmail.com>
2020-11-03 13:25:10 +0100

    feat: changed Request Handling to Controller

--- src/routes/scientist.ts
+++ src/routes/scientist.ts
@@ -1,31 +1,35 @@
 import express, { NextFunction } from 'express'
 import { Request, Response} from 'express'
+import { nextTick } from 'process'
+import { ScientistController } from '../controller/scientist'
 import Scientist from '../db/schemas/scientist'
 
 
 let Router = express.Router()
 
+let SciController = new ScientistController(); 
 
 Router.get("/", (req: Request, res: Response) => {
+    SciController.getScientists(req, res);
+})
 
-    Scientist.find(function (err: any, scientist: Document) {
-        res.send(scientist)
-    })
+
+Router.get("/:scientistId", (req: Request, res: Response) => {
+    SciController.getScientists(req, res);
 })
 
 Router.post("/", (req: Request, res:Response) => {
+    SciController.createScientist(req, res);
+    
+})
 
-    const scientist = new Scientist(req.body);
+Router.delete("/:scientistId", (req: Request, res:Response) => {
+    SciController.deleteScientist(req, res)
+})
 
-    scientist.save((err: any, sci: Document) => {
-        if (err) {
-            res.status(400)
-            res.send(err)
-        }else{
-            res.status(201)
-            res.send(sci)
-        }
-    })
+
+Router.put("/:scientistId", (req: Request, res:Response) => {
+    SciController.updateScientist(req, res)
 })
 
 export default Router
